5bed4aad447663c80fc3dfdddcdf49a6ed3bb60d
murasame112 <tomaszwiesek00@gmail.com>
2024-12-31 20:48:18 +0100

    react setup

---
+++ VictorifyApi/VictorifyFrontend/src/components/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+import stylesDefault from '../../index.css';
+import styles from './Footer.module.css';
+
+function Footer() {
+    return (
+        <div className={styles.footer}>
+            <div className={styles.content}>
+                <p>Web Design by Tomasz Więsek, 2024</p>
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
---
+++ VictorifyApi/VictorifyFrontend/src/components/Header/Header.tsx
@@ -0,0 +1,25 @@
+import stylesDefault from '../../index.css';
+import styles from './Header.module.css';
+// import {
+// 	Link,
+// 	useLocation
+// } from 'react-router-dom';
+
+function Header() {
+  //const location = useLocation();
+	// let className = 'navbarTemp';
+  // if (location.pathname !== '/'){
+	// 	className += 'Inactive'
+	// }
+	return (	
+	// <div className={className}>
+		<p>HEADER</p>
+		// <br/>
+		/* <Link to='/strategies'>Strategy list</Link><br/>
+		<Link to='/create'>Add new strategy</Link><br/>
+		<Link to='/parse'>build parser</Link> */
+	// </div>
+);
+}
+
+export default Header;
